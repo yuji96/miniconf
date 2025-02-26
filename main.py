d73e84c7874b3e8976a82cda6a8255cd7c17b6e4
yuji96 <y.yama683@gmail.com>
2025-02-26 18:43:49 +0900

    disable cache

--- main.py
+++ main.py
@@ -21,6 +21,8 @@ by_uid: ByUid = None
 
 app = Flask(__name__)
 app.config.from_object(__name__)
+app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 freezer = Freezer(app)
 markdown = Markdown(app)
 
